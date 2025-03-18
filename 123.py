df074f18b487e2d99d72093e6ee4d3790a7bf828
Yuxuan He <2990346h@student.gla.ac.uk>
2025-03-18 14:41:06 +0000

    modify

--- 123.py
+++ 123.py
@@ -1,22 +1,22 @@
 def check_sensitive_words(text, sensitive_words):
-    text = text.lower()  # 将文本转换为小写  
+    text = text.lower()
     for word in sensitive_words:
-        if word in text and word != '':  # 检查不区分大小写的敏感词
+        if word in text and word != '':
             print("Error: Sensitive word found:", word)
-            return 'true'  # 找到敏感词后返回true  
-    return 'false'  # 如果没有找到敏感词，循环结束后返回false  
+            return 'true'
+    return 'false'
 
 
-# 读取敏感词列表（这里假设敏感词列表在sensitive_words.txt文件中）
+# sensitive_words.txt）
 with open('static/sensitive_words/words.txt', 'r', encoding='utf-8') as f:
-    sensitive_words = [line.strip().lower() for line in f]  # 读取时转换为小写  
+    sensitive_words = [line.strip().lower() for line in f]
 
-# 测试文本  
-test_text = "你去死吧"
 
-# 检测敏感词  
+test_text = "badword1"
+
+
 result = check_sensitive_words(test_text, sensitive_words)
 if result == 'true':
-    print('有')
+    print('yes')
 else:
-    print('没有')
+    print('no')
